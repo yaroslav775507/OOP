26455f12043c35ceee094667756719c4d4e923d2
yaroslav775507 <redyar55@gmail.com>
2023-05-15 17:08:44 +0300

    Вывод информации о машинах ООП

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Вывод информации о машинах
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        ArrayList<Car> arrayList = new ArrayList<>();
+        String[] s;
+        for (int i = 0; i < n; i++) {
+            s = sc.nextLine().split(" ");
+            arrayList.add(new Car(s[0], s[1], Integer.parseInt(s[2])));
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.println(arrayList.get(i).getInfo());
+        }
+
     }
 }
